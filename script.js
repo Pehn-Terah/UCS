b3cfee451bdbfc69675a2503bf8ad598f8d06a5c
Pehn-Terah <samsonisijola@gmail.com>
2024-12-17 15:42:53 +0100

    Let's see again o

--- script.js
+++ script.js
@@ -4,19 +4,30 @@ document.addEventListener('DOMContentLoaded', () => {
     const navLinks = document.querySelectorAll('.nav-links li');
 
     // Hamburger menu toggle
-    burger.addEventListener('click', () => {
+    const navSlide = () => {
+      const burger = document.querySelector('.burger');
+      const nav = document.querySelector('.nav-links');
+      const navLinks = document.querySelectorAll('.nav-links li');
+
+      burger.addEventListener('click', () => {
+        // Toggle Nav
         nav.classList.toggle('nav-active');
 
+        // Animate Links
         navLinks.forEach((link, index) => {
-            if (link.style.animation) {
-                link.style.animation = '';
-            } else {
-                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
-            }
+          if (link.style.animation) {
+            link.style.animation = '';
+          } else {
+            link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+          }
         });
 
+        // Burger Animation
         burger.classList.toggle('toggle');
-    });
+      });
+    }
+
+    navSlide();
 
     // Service pop-up functionality
     const serviceButtons = document.querySelectorAll('.service-button');
