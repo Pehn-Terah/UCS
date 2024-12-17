69f77c509a235f9b0ac4bed3cc0cdd7fd57acc39
Pehn-Terah <samsonisijola@gmail.com>
2024-12-17 15:51:09 +0100

    please no mess up

--- script.js
+++ script.js
@@ -4,30 +4,19 @@ document.addEventListener('DOMContentLoaded', () => {
     const navLinks = document.querySelectorAll('.nav-links li');
 
     // Hamburger menu toggle
-    const navSlide = () => {
-      const burger = document.querySelector('.burger');
-      const nav = document.querySelector('.nav-links');
-      const navLinks = document.querySelectorAll('.nav-links li');
-
-      burger.addEventListener('click', () => {
-        // Toggle Nav
+    burger.addEventListener('click', () => {
         nav.classList.toggle('nav-active');
 
-        // Animate Links
         navLinks.forEach((link, index) => {
-          if (link.style.animation) {
-            link.style.animation = '';
-          } else {
-            link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
-          }
+            if (link.style.animation) {
+                link.style.animation = '';
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+            }
         });
 
-        // Burger Animation
         burger.classList.toggle('toggle');
-      });
-    }
-
-    navSlide();
+    });
 
     // Service pop-up functionality
     const serviceButtons = document.querySelectorAll('.service-button');
